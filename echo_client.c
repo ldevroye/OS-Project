e45f469a79bffb1bf6748c4a80137f32953ca0fd
Dark Peace <74102789+rbecker5@users.noreply.github.com>
2021-12-06 15:36:04 +0100

    Add files via upload

---
+++ echo_client.c
@@ -0,0 +1,84 @@
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "common.h"
+
+int main(int argc, char const *argv[]) {
+  int sock = checked(socket(AF_INET, SOCK_STREAM, 0));
+  struct sockaddr_in serv_addr;
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(8080);
+
+  // Conversion de string vers IPv4 ou IPv6 en binaire
+  checked(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr));
+  checked(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)));
+
+
+  pthread_t tids[1];  // thread ids
+  pthread_create(&tids[0], NULL, recv_messages, &a);
+  
+
+
+
+  char buffer[1024];
+  ssize_t nbytes = 1;
+  while (nbytes > 0 && fgets(buffer, 1024, stdin)) {
+    // Supprimer le \n
+    size_t len = strlen(buffer);
+    buffer[len - 1] = '\0';
+    // On garde la même taille de string pour explicitement envoyer le '\0'
+    nbytes = ssend(sock, buffer, len);
+    if (nbytes > 0) {
+      char *recvbuffer;
+      nbytes = receive(sock, (void *)&recvbuffer);
+      if (nbytes > 0) {
+        printf("Client received %s\n", recvbuffer);
+        free(recvbuffer);
+      }
+    }
+  }
+  return 0;
+}
+
+void recv_messages() {
+  
+}
+
+
+/*
+
+int main(int argc, char const *argv[]) {
+  int sock = checked(socket(AF_INET, SOCK_STREAM, 0));
+  struct sockaddr_in serv_addr;
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(8080);
+
+  // Conversion de string vers IPv4 ou IPv6 en binaire
+  checked(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr));
+
+  checked(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)));
+  char buffer[1024];
+  ssize_t nbytes = 1;
+  while (nbytes > 0 && fgets(buffer, 1024, stdin)) {
+    // Supprimer le \n
+    size_t len = strlen(buffer);
+    buffer[len - 1] = '\0';
+    // On garde la même taille de string pour explicitement envoyer le '\0'
+    nbytes = ssend(sock, buffer, len);
+    if (nbytes > 0) {
+      char *recvbuffer;
+      nbytes = receive(sock, (void *)&recvbuffer);
+      if (nbytes > 0) {
+        printf("Client received %s\n", recvbuffer);
+        free(recvbuffer);
+      }
+    }
+  }
+  return 0;
+}
+
+*/
---
+++ select_echo_server.c
@@ -0,0 +1,128 @@
+// Example code: A simple server side code, which echos back the received message.
+// Handle multiple socket connections with select and fd_set on Linux
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>     //strlen
+#include <sys/socket.h>
+#include <sys/time.h>   //FD_SET, FD_ISSET, FD_ZERO macros
+#include <unistd.h>     //close
+
+#include "common.h"
+
+int main(int argc, char *argv[]) {
+  int opt = 1;
+  int master_socket = checked(socket(AF_INET, SOCK_STREAM, 0));
+  checked(setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)));
+
+  // type of socket created
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(8080);
+  checked(bind(master_socket, (struct sockaddr *)&address, sizeof(address)));
+  checked(listen(master_socket, 3));
+
+  size_t addrlen = sizeof(address);
+  fd_set readfds;
+  int clients[1024];
+  int nclients = 0;
+  while (true) {
+    FD_ZERO(&readfds);
+    FD_SET(master_socket, &readfds);
+    int max_fd = master_socket;
+    for (int i = 0; i < nclients; i++) {
+      FD_SET(clients[i], &readfds);
+      if (clients[i] > max_fd) {
+        max_fd = clients[i];
+      }
+    }
+    // wait for an activity on one of the sockets, timeout is NULL
+    select(max_fd + 1, &readfds, NULL, NULL, NULL);
+
+    if (FD_ISSET(master_socket, &readfds)) {
+      // Si c'est le master socket qui a des donnees, c'est une nouvele connexion.
+      clients[nclients] = accept(master_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+      nclients++;
+    } else {
+      // Sinon, c'est un message d'un client
+      for (int i = 0; i < nclients; i++) {
+        if (FD_ISSET(clients[i], &readfds)) {
+          char *buffer;
+          size_t nbytes = receive(clients[i], (void *)&buffer);
+          if (nbytes > 0) {  // closed
+            ssend(clients[i], buffer, nbytes);
+            free(buffer);
+          } else {
+            close(clients[i]);
+            // On deplace le dernier socket a la place de libre pur ne pas faire de trou.
+            clients[i] = clients[nclients - 1];
+            nclients--;
+          }
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+/*
+
+int main(int argc, char *argv[]) {
+  int opt = 1;
+  int master_socket = checked(socket(AF_INET, SOCK_STREAM, 0));
+  checked(setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)));
+
+  // type of socket created
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(8080);
+  checked(bind(master_socket, (struct sockaddr *)&address, sizeof(address)));
+  checked(listen(master_socket, 3));
+
+  size_t addrlen = sizeof(address);
+  fd_set readfds;
+  int clients[1024];
+  int nclients = 0;
+  while (true) {
+    FD_ZERO(&readfds);
+    FD_SET(master_socket, &readfds);
+    int max_fd = master_socket;
+    for (int i = 0; i < nclients; i++) {
+      FD_SET(clients[i], &readfds);
+      if (clients[i] > max_fd) {
+        max_fd = clients[i];
+      }
+    }
+    // wait for an activity on one of the sockets, timeout is NULL
+    select(max_fd + 1, &readfds, NULL, NULL, NULL);
+
+    if (FD_ISSET(master_socket, &readfds)) {
+      // Si c'est le master socket qui a des donnees, c'est une nouvele connexion.
+      clients[nclients] = accept(master_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+      nclients++;
+    } else {
+      // Sinon, c'est un message d'un client
+      for (int i = 0; i < nclients; i++) {
+        if (FD_ISSET(clients[i], &readfds)) {
+          char *buffer;
+          size_t nbytes = receive(clients[i], (void *)&buffer);
+          if (nbytes > 0) {  // closed
+            ssend(clients[i], buffer, nbytes);
+            free(buffer);
+          } else {
+            close(clients[i]);
+            // On deplace le dernier socket a la place de libre pur ne pas faire de trou.
+            clients[i] = clients[nclients - 1];
+            nclients--;
+          }
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+*/
